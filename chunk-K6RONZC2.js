3ebb48dd73513fc41511567cfc6706d96bc3cb41
ejprietocal <107762092+ejprietocal@users.noreply.github.com>
2025-03-27 05:05:24 +0000

    Deploying to teslo_preview from @ ejprietocal/teslo_front@b29a93c45df9e5ddd528ebefccbbc54ba78de046 🚀

--- chunk-K6RONZC2.js
+++
@@ -1,1 +0,0 @@
-import{Da as e,Ua as t,Va as p,ib as s}from"./chunk-OM5ZFAIQ.js";var i=class o{static \u0275fac=function(n){return new(n||o)};static \u0275cmp=e({type:o,selectors:[["app-not-found"]],decls:2,vars:0,template:function(n,m){n&1&&(t(0,"p"),s(1,"not-found works!"),p())},encapsulation:2})};export{i as NotFoundComponent};
