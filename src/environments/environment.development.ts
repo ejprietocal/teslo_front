47521fc2a2c1103b94c08938bea5f3e55e7a63b7
Eider Prieto <prietoeider1@gmail.com>
2025-04-27 07:06:29 -0500

    Dejo de trackear environments

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-  URL_LOGIN_LOCAL: 'http://localhost:3000/api/auth/login',
-  URL_LOGIN: 'https://tesloback-production.up.railway.app/api/auth/login',
-  URL_REGISTER : 'https://tesloback-production.up.railway.app/api/register',
-  URL_CHECK_TOKEN : 'https://tesloback-production.up.railway.app/api/auth/check-status',
-};
