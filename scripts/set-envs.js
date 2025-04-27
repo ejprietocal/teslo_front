ee7d07dd135176ad4c00c362b0e9148937bdb82a
Eider Prieto <prietoeider1@gmail.com>
2025-04-27 06:59:56 -0500

    creacion de environments por script

--- scripts/set-envs.js
+++ scripts/set-envs.js
@@ -6,13 +6,16 @@ const targetPathDev = './src/environments/environment.development.ts';
 
 const targetPathEnv = '.env';
 
-if (!process.env.URL_LOGIN_LOCAL) {
-  throw new Error('URL_LOGIN_LOCAL no definido');
+if (!process.env.URL_LOGIN_LOCAL || !process.env.URL_LOGIN || !process.env.URL_REGISTER || !process.env.URL_CHECK_TOKEN) {
+  throw new Error('Alguna de las variables de entorno no estan definidas');
 }
 
 const envContent = `
 export const environment = {
-  URL_LOGIN_LOCAL: '${process.env['URL_LOGIN_LOCAL']}'
+  URL_LOGIN_LOCAL: '${process.env['URL_LOGIN_LOCAL']}',
+  URL_LOGIN: '${process.env['URL_LOGIN']}',
+  URL_REGISTER : '${process.env['URL_REGISTER']}',
+  URL_CHECK_TOKEN : '${process.env['URL_CHECK_TOKEN']}',
 };
 `
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
-  production : true,
+  URL_LOGIN_LOCAL: 'http://localhost:3000/api/auth/login',
   URL_LOGIN: 'https://tesloback-production.up.railway.app/api/auth/login',
   URL_REGISTER : 'https://tesloback-production.up.railway.app/api/register',
-  URL_CHECK_TOKEN : 'https://tesloback-production.up.railway.app/api/auth/check-status'
+  URL_CHECK_TOKEN : 'https://tesloback-production.up.railway.app/api/auth/check-status',
 };
