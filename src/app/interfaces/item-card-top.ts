68fc25ce79036437a3089fc31efce7161d778ecd
Eider Prieto <prietoeider1@gmail.com>
2025-05-01 17:11:02 -0500

    Creacion de tarjetas superiores

---
+++ src/app/interfaces/item-card-top.ts
@@ -0,0 +1,7 @@
+export interface ItemCardTop {
+  title: string;
+  value: number;
+  percentage: number;
+  icon: string;
+  message?: string;
+}
--- src/app/views/components/content-internal/content-internal.component.ts
+++ src/app/views/components/content-internal/content-internal.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TopMenuComponent } from '../top-menu/top-menu.component';
+import { TopCardComponent } from '../top-card/top-card.component';
+import { ItemCardTop } from 'src/app/interfaces/item-card-top';
 
 @Component({
   selector: 'app-content-internal',
   imports: [
-    TopMenuComponent
+    TopMenuComponent,
+    TopCardComponent
   ],
   templateUrl: './content-internal.component.html',
   styleUrl: './content-internal.component.css'
 })
-export class ContentInternalComponent {
+export class ContentInternalComponent implements OnInit {
 
+  items: ItemCardTop[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        title: 'Ingreso hoy',
+        value: 100,
+        percentage: .23,
+        icon: 'faMoneyBill',
+        message: 'vs dia anterior'
+      },
+      {
+        title: 'Total domicilios',
+        value: 10567345,
+        percentage: -.12,
+        icon: 'faMotorcycle',
+        message: 'vs dia anterior'
+      },
+      {
+        title: 'Total local',
+        value: 300,
+        percentage: .14,
+        icon: 'faStore',
+        message: 'vs dia anterior'
+      },
+      {
+        title: 'Ingreso acumulado',
+        value: 400,
+        percentage: .11,
+        icon: 'faMoneyBillTrendUp',
+        message: 'vs mes anterior'
+
+      },
+    ];
+  }
 }
