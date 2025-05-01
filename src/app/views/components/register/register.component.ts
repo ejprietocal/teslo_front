fbb451302248c9b4216820daaebefe384b80775e
jeinerarenas <jeinerarenas435@gmail.com>
2025-05-01 17:20:48 -0500

    cambios de registro

--- src/app/views/components/register/register.component.ts
+++ src/app/views/components/register/register.component.ts
@@ -3,16 +3,27 @@ import { Component, inject, Signal, signal } from '@angular/core';
 import { PasswordModule } from 'primeng/password';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { User } from '../../../interfaces/user';
 import { Router } from '@angular/router';
-
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReCaptchaV3Service } from 'ng-recaptcha-2';
 
 import { ToastModule } from 'primeng/toast';
 import { RegisterService } from '../../services/register.service';
+import { Select } from 'primeng/select';
+import { Checkbox } from 'primeng/checkbox';
+
+
+interface City {
+    name: string;
+    id: number;
+}
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [
     CommonModule,
     PasswordModule,
@@ -21,6 +32,8 @@ import { RegisterService } from '../../services/register.service';
     FormsModule,
     ReactiveFormsModule,
     ToastModule,
+    FontAwesomeModule,
+    Select,Checkbox
 
   ],
   templateUrl: './register.component.html',
@@ -28,23 +41,28 @@ import { RegisterService } from '../../services/register.service';
 })
 export class RegisterComponent {
 
+  public datosUser = signal<User | null>(null);
+
+  fb = inject(FormBuilder);
+  recaptchaService = inject(ReCaptchaV3Service);
+
   constructor (
     private readonly registerService: RegisterService,
     private readonly router: Router
   ) { }
-
+  cities: City[] | undefined;
+  selectedCity: City | undefined;
+  checked: boolean = false;
   nombre: string = '';
   email: string = '';
   apellidos: string = '';
   cedula: number = 0;
   telefono: string = '';
-  ciudad: string = '';
   password: string = '';
-  confermar_password: string = '';
-
-  public datosUser = signal<User | null>(null);
-
-  fb = inject(FormBuilder);
+  confirmar_password: string = '';
+  mostrarPassword: boolean = false;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
 
   form: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -52,18 +70,48 @@ export class RegisterComponent {
     apellidos: ['', [Validators.required]],
     cedula: ['', [Validators.required, Validators.minLength(6)]],
     password: ['', [Validators.required, Validators.minLength(6)]],
-    confermar_password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmar_password: ['', [Validators.required, Validators.minLength(6)]],
     telefono: ['', [Validators.required, Validators.minLength(10)]],
-    ciudad: ['', [Validators.required]],
-  });
+    nombre_negocio: ['', [Validators.required]],
+    tipo_negocio: ['', [Validators.required]],
+    checked:['', [Validators.required]],
+    recaptchaToken: ['']
+  },{ validators: this.passwordsIguales('password', 'confirmar_password') });
 
   registrarData(): void {
-    console.log('llego al componente');
     if (this.form.valid) {
-      this.registerService.registrarData_service(this.form);
+        this.recaptchaService.execute('registrarData').subscribe((token) =>{
+        this.form.get('recaptchaToken')?.setValue(token);
+        this.registerService.registrarData_service(this.form);
+      })
     }else{
       this.form.value
     }
   }
+
+  passwordsIguales(pass1: string, pass2: string): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const form = formGroup as FormGroup;
+      const password = form.get(pass1)?.value;
+      const confirmarPassword = form.get(pass2)?.value;
+
+      if (password === confirmarPassword) {
+        return null; // No hay error
+      } else {
+        return { passwordsNoCoinciden: true }; // Hay error
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.cities = [
+        { name: 'Ferreterias', id: 1 },
+        { name: 'Restaurantes o Comidas rápidas', id: 2 },
+        { name: 'Tienda de barrio', id: 3 },
+        { name: 'Farmacia o droguería', id: 4 },
+        { name: 'Panadería', id: 5 },
+        { name: 'Ropa, Calzado y Accesorios', id: 6 }
+    ];
+}
 
 }
--- src/app/views/services/register.service.ts
+++ src/app/views/services/register.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { User } from '../../interfaces/user';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { ActivateLoaderService } from '../../services/activate-loader.service';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +15,37 @@ export class RegisterService {
   constructor(
     private http : HttpClient,
     private router : Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private readonly activateLoader : ActivateLoaderService,
   ) { }
 
   public datosUser = signal<User | null>(null);
+  Login = inject(LoginService);
+
 
   registrarData_service(form:FormGroup):void{
+    this.activateLoader.activateSignal();
     console.log(form.value);
+    return;
+    this.http.post<any>('https://tesloback-production.up.railway.app/api/register', form.value).subscribe({
+      next: (res) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Registro exitoso',
+          detail: 'Ahora puedes iniciar sesión'
+        })
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: err.error?.message || 'Ocurrió un error al registrarse'
+      });
+      this.activateLoader.deactivateSignal();
+
+      }
+    });
   }
 
 }
