31d50e70f05c7b81727b9afbedab23887270f0ed
Eider Prieto <prietoeider1@gmail.com>
2025-05-06 09:51:46 -0500

    Update

--- src/app/views/services/register.service.ts
+++ src/app/views/services/register.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
-import { User } from '../../interfaces/user';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { TipoNegocio } from 'src/app/interfaces/tipo-negocio';
 import { DataRegister } from 'src/app/interfaces/data-register';
@@ -11,10 +9,8 @@ import { DataRegister } from 'src/app/interfaces/data-register';
 })
 export class RegisterService {
 
-  public datosUser = signal<User | null>(null);
-  private readonly API_URL = environment.URL_REGISTER; // variable de entorno
-
-  constructor(private http: HttpClient) {}
+  private readonly API_URL = environment.URL_REGISTER;
+  private readonly http = inject(HttpClient);
 
   registrarData_service(data_service: DataRegister) {
     console.log(data_service);
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,6 @@ export const environment = {
   URL_LOGIN: 'https://tesloback-production.up.railway.app/api/auth/login',
   URL_REGISTER : 'https://tesloback-production.up.railway.app/api/auth/register',
   URL_CHECK_TOKEN : 'https://tesloback-production.up.railway.app/api/auth/check-status',
+  URL_BUSINESS_TYPES : 'https://tesloback-production.up.railway.app/api/business/business_types',
   CLAVE_SITIO_WEB_RECAPTCHA: '6LfCcSYrAAAAAOvE3WWa8whL_76ZL6BAaoS-Fzxu',
 };
