6dc1c6fe328c0b7e2e295216664f9e8bb13e6568
jeinerarenas <jeinerarenas435@gmail.com>
2025-05-16 08:48:52 -0500

    cambios x

---
+++ src/app/interfaces/productos.ts
@@ -0,0 +1,9 @@
+export interface Productos {
+  nameProducto: string,
+  idProducto: number,
+  precio: number,
+  descripcion:string,
+  idCategoria: number,
+  categoria: string
+
+}
--- src/app/views/components/products/products.component.ts
+++ src/app/views/components/products/products.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    FontAwesomeModule,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export default class ProductsComponent {
+
 }
