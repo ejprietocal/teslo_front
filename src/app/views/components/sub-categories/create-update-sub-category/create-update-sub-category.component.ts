cc50607281420e74f15200127bacf3cd61f69e7d
Eider Prieto <prietoeider1@gmail.com>
2025-05-23 18:10:39 -0500

    cambios guardados

--- src/app/views/components/sub-categories/create-update-sub-category/create-update-sub-category.component.ts
+++ src/app/views/components/sub-categories/create-update-sub-category/create-update-sub-category.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Category } from 'src/app/interfaces/category';
 
 @Component({
   selector: 'app-create-update-sub-category',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './create-update-sub-category.component.html',
   styleUrl: './create-update-sub-category.component.css'
 })
 export class CreateUpdateSubCategoryComponent {
+
+  @Input({ required: true }) mode!: WritableSignal<string>;
+  @Input({ required: true }) categories!: Category[];
+
+  fb = inject(FormBuilder);
+
+  textModalmodify = {
+    title : "Modificar SubCategoria",
+    button : "Modificar"
+  }
+  textModalCreate = {
+    title : "Crear SubCategoria",
+    button : "Crear"
+  }
+
+  ngOnInit(): void {
+    console.log(this.categories);
+  }
+
+  form: FormGroup = this.fb.group({
+    category_product_id: [Number, [Validators.required]],
+    description: ['', [Validators.required, Validators.minLength(3)]],
+    isActive: [ true ,Validators.required],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    slug : ['', Validators.required],
+  });
 
 }
--- src/app/views/components/sub-categories/sub-categories.component.ts
+++ src/app/views/components/sub-categories/sub-categories.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { CreateUpdateSubCategoryComponent } from './create-update-sub-category/create-update-sub-category.component';
 import { DeleteSubCategoryComponent } from './delete-sub-category/delete-sub-category.component';
+import { DataTable } from 'simple-datatables';
+import { ActivateLoaderService } from 'src/app/services/activate-loader.service';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from '../../services/category.service';
+import { Category } from 'src/app/interfaces/category';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sub-categories',
@@ -13,4 +19,27 @@ import { DeleteSubCategoryComponent } from './delete-sub-category/delete-sub-cat
 })
 export default class SubCategoriesComponent {
 
+  @ViewChild('subCategoryWrapper') subCategory!: ElementRef;
+  public mode = signal<string>('');
+  public dataTableInstance?: DataTable;
+  private readonly loaderService = inject(ActivateLoaderService);
+  private readonly toastr = inject(ToastrService);
+  public readonly categoryService = inject(CategoryService);
+  public categories : Category[] = [];
+  public getCategoriesSuscription?: Subscription;
+
+  ngAfterViewInit(): void {
+    this.getCategoriesSuscription = this.categoryService.getCategories().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.categories = res;
+      },
+    });
+  }
+
+  constructor(){
+    effect(() => {
+
+    });
+  }
 }
